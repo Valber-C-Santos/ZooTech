c32a5cdacbc12e44d1ca00372713746ecbefaa32
Valber Candido <valber_558@hotmail.com>
2023-06-14 19:02:00 -0300

    Requisito-3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const nome = data.employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+  return nome;
 };
 
 module.exports = getEmployeeByName;
