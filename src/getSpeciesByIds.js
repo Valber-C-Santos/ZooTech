065cbc42d6cec9e5dde749eea8d798af85e4e54a
Valber Candido <valber_558@hotmail.com>
2023-06-14 15:50:00 -0300

    Requisito-1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const arrVazio = [];
+  if (ids.length === 0) {
+    return arrVazio;
+  }
+  return ids.map((aidi) => data.species.find((especie) => especie.id === aidi));
 };
 
 module.exports = getSpeciesByIds;
