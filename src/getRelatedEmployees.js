896b1d2da972e7b7ed771ad180915e2a376a90c4
Valber Candido <valber_558@hotmail.com>
2023-06-15 14:49:42 -0300

    Requisito-4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees
+  .some((teste) => teste.managers
+    .find((aidi) => aidi === id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((gerente) => gerente.managers.includes(managerId))
+    .map((nome) => `${nome.firstName} ${nome.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
