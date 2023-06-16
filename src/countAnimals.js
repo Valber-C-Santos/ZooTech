abe87e03b775d2f86fe241476ec028c948bf20fd
Valber Candido <valber_558@hotmail.com>
2023-06-16 19:06:00 -0300

    Requisito-5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, especie) => {
+      acc[especie.name] = especie.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.species && !animal.sex) {
+    return data.species
+      .find((especie) => especie.name === animal.species).residents.length;
+  }
+  return data.species.find((especie) => especie.name === animal.species)
+    .residents.filter((bicho) => bicho.sex === animal.sex).length;
 };
 
 module.exports = countAnimals;
