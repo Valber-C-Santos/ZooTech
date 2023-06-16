cd169a4653595a5b9d52e980a4da3422d302d350
Valber Candido <valber_558@hotmail.com>
2023-06-16 20:55:44 -0300

    Requisito-10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const empregado = data.employees.find((colaborador) => colaborador.id === id).responsibleFor[0];
+  const animalVelho = data.species.find((teste) => teste.id === empregado).residents;
+  return Object.values(animalVelho.sort((a, b) => b.age - a.age)[0]);
 };
 
 module.exports = getOldestFromFirstSpecies;
