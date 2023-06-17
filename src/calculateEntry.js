697bab3ea105c505ab364bb3349172903ca09a6a
Valber Candido <valber_558@hotmail.com>
2023-06-16 22:28:47 -0300

    Requisito-8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((p) => p.age < 18).length;
+
+  const adult = entrants.filter((p) => p.age >= 18 && p.age < 50).length;
+
+  const senior = entrants.filter((p) => p.age >= 50).length;
+
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) return 0;
+
+  const visitantes = countEntrants(entrants);
+
+  const preçoCriança = visitantes.child * data.prices.child;
+
+  const preçoAdulto = visitantes.adult * data.prices.adult;
+
+  const preçoIdoso = visitantes.senior * data.prices.senior;
+
+  const soma = preçoCriança + preçoAdulto + preçoIdoso;
+
+  return soma;
 };
 
 module.exports = { calculateEntry, countEntrants };
